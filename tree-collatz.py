5eb3891e4d83fbc03c25d52ed161f1ba1c95e31b
Aditya P Venugopal <vipremigini@gmail.com>
2024-07-17 16:20:05 +0530

    Update tree-collatz.py

--- tree-collatz.py
+++ tree-collatz.py
@@ -3,22 +3,22 @@ import turtle
 def collatzer(y,listac):
     global listc
     if y == 0:
-        print(0)
+        return
     elif y == 1:
         return listac
     elif y % 2 == 0:
         y = int(y / 2)
         listc.append(y)
-        print(y)
+        
         collatzer(y,listac)
     else:
         y = (y * 3) + 1
         listc.append(y)
-        print(y)
+        
         collatzer(y,listac)
 
 listx = []
-for i in range(51):
+for i in range(500):
     listc = [i]
     collatzer(i, listc)
     listx.append(listc)
@@ -27,10 +27,33 @@ print(listx)
 
 t = turtle.Turtle()
 
-points = {}
+points = {1:(-150,0)}
 
-for i in listx():
+for i in listx:
     t.up()
+    prev = 1
+    ang = t.heading()
+    print(ang)
+    #t.right(ang)
+    for num in i[::-1][:30]:
+        if num in points:
+            prev = num
+            
+        else:
+            t.up()
+            t.goto(points[prev])
+            t.down()
+            if prev * 2 == num:
+        
+                t.right(35)
+                t.forward(20)
+                prev = num
+                points[num] = t.position()
+                
+            else:
+                t.left(40)
+                t.forward(20)
+                prev = num
+                points[num] = t.position()
+            
     
-    
-
