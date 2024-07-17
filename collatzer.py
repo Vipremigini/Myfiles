483b0a0ae1d3131cebde3aaced0eab110c24bbf5
Aditya P Venugopal <vipremigini@gmail.com>
2024-07-17 15:13:58 +0530

    Update collatzer.py

--- collatzer.py
+++ collatzer.py
@@ -3,29 +3,40 @@ import math
 
 t = turtle.Turtle()
 t.up()
-t.goto(-400,0)
+t.goto(-400,-350)
 t.down()
 ya = 0
 x = int(input("enter a number"))
 print(x)
+listc = [x]
 
 def collatzer(y):
-    global ya
+    global listc
     if y == 0:
         print(0)
     elif y == 1:
-        print(1)
+
     elif y % 2 == 0:
-        pos = t.position()
         y = int(y / 2)
-        t.goto(pos[0] + 1, math.log(y,1.2))
+        listc.append(y)
         print(y)
         collatzer(y)
     else:
-        pos = t.position()
         y = (y * 3) + 1
-        t.goto(pos[0] + 1, math.log(y,1.2))
+        listc.append(y)
         print(y)
         collatzer(y)
 
 collatzer(x)
+
+def grapher(l):
+    maxn = max(l)
+    bas = math.pow(maxn, 1/700)
+    leng = len(l)
+    stp = 800/leng
+    ya = 0
+    for i in l:
+        pos = t.position()
+        t.goto(pos[0] + stp, -350 + math.log(i,bas))
+
+grapher(listc)
