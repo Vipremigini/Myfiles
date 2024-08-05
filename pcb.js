a2b07fe6e88ff9a4e613babc1cbc0a3aff9d2dab
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-05 17:03:06 +0530

    Update pcb.js

--- pcb.js
+++ pcb.js
@@ -8,31 +8,49 @@ const finalLines = [];
 
 
 const t = new bt.Turtle()
-const p = new bt.Turtle()
+let p = []
 
 function expr(h, w, x, y, tur) {
-  p.setAngle(0)
-  p.jump([x,y])
-  p.forward(w)
-  p.left(90)
-  p.forward(h)
-  p.left(90)
-  p.forward(w)
-  p.left(90)
-  p.forward(h)
-  
+  t.jump([x,y])
+  for (let i = 0; (i < h / 0.2); i++) {
+  t.forward(w)
+  t.left(90)
+  t.forward(0.1)
+  t.left(90)
+  t.forward(w)
+  t.right(90)
+  t.forward(0.1)
+  t.right(90)
+}
+}
+function pad(r,x,y){
+  t.jump([x,y-r])
+  t.arc(360, r)
+  t.goTo([x,(y - r)  + 0.1])
+  t.arc(360, r-0.1)
+  t.goTo([x,(y - r)  + 0.2])
+  t.arc(360, r-0.2)
+   
+  }
+
+
+function trace(x1,y1,x2,y2) {
+  let tr = [
+    [x1,y1],
+    [x2,y2]
+  ]
+p.push(tr)
+ let tr1 = [
+    [x1 - 0.3,y1],
+    [x2 - 0.3,y2]
+  ]
+  p.push(tr1)
 }
 
-for (let i = 0; i < 625; i++) {
-  t.forward(125)
-  t.left(90)
-  t.forward(0.1)
-  t.left(90)
-  t.forward(125)
-  t.right(90)
-  t.forward(0.1)
-  t.right(90)
-}
+trace(3,2,24,15)
+pad(0.785,24,18)
+pad(1,27,18)
+pad(0.5,24,15)
 expr(3.302,3.048,1.143,0.762,p)
 expr(3.3,1.2,6.096,0.635,p)
 expr(3.3,1.2,10.161,0.635,p)
@@ -52,6 +70,7 @@ expr(3.3,1.2,16.129,16.637,p)
 expr(3.3,1.2,18.161,16.637,p)
 expr(3.3,1.2,20.066,16.637,p)
 
-let sc = bt.cut(t.lines(),p.lines())
-const xyz = bt.merge(sc,p.lines())
-drawLines(xyz);
+//let sc = bt.cut(t.lines(),p.lines())
+//const xyz = bt.merge(sc,p.lines())
+drawLines(t.lines());
+drawLines(p);
