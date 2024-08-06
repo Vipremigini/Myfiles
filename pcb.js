c7326e2d17fc20e09e64fb6c2435f28c52e895e9
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-06 07:21:14 +0530

    Update pcb.js

--- pcb.js
+++ pcb.js
@@ -9,6 +9,8 @@ const finalLines = [];
 
 const t = new bt.Turtle()
 let p = []
+let r = new bt.Turtle()
+
 
 function expr(h, w, x, y, tur) {
   t.jump([x,y])
@@ -38,6 +40,7 @@ function expr(h, w, x, y, tur) {
 }
 }
 function pad(r,x,y){
+  t.setAngle(0)
   t.jump([x,y-r])
   t.arc(360, r)
   t.goTo([x,(y - r)  + 0.1])
@@ -46,20 +49,65 @@ function pad(r,x,y){
    
   }
 
+function rrect(h, w, x, y) {
+  t.jump([x,y])
+  t.setAngle(0)
+  for (let i = 0; (i < h / 0.2); i++) {
+  t.forward(w)
+  t.left(90)
+  t.forward(0.1)
+  t.left(90)
+  t.forward(w)
+  t.right(90)
+  t.forward(0.1)
+  t.right(90)
+}
+  t.jump([x,y])
+  t.setAngle(90)
+  for (let i = 0; (i < w / 0.2); i++) {
+  
+  t.forward(h)
+  t.right(90)
+  t.forward(0.1)
+  t.right(90)
+  t.forward(h)
+  t.left(90)
+  t.forward(0.1)
+  t.left(90)
+}
+  r.setAngle(0)
+  r.jump([x + (w/2) ,y + h - w/2])
+  r.arc(360, w/2)
+  r.jump([x + (w/2),y+h - w/2 + 0.1])
+  r.arc(360, w/2- 0.1)
+  r.jump([x + (w/2),y+h - w/2 + 0.2])
+  r.arc(360, w/2- 0.2)
+  r.jump([x + (w/2),y+h - w/2 + 0.4])
+  r.arc(360, w/2- 0.4)
 
+  r.setAngle(0)
+  r.jump([x + (w/2) ,y - w/2])
+  r.arc(360, w/2)
+  r.jump([x + (w/2),y - w/2 + 0.1])
+  r.arc(360, w/2- 0.1)
+  r.jump([x + (w/2),y - w/2 + 0.2])
+  r.arc(360, w/2- 0.2)
+  r.jump([x + (w/2),y - w/2 + 0.4])
+  r.arc(360, w/2- 0.4)
+}
 function trace(x1,y1,x2,y2) {
   let tr = [
-    [x1-0.15,y1-0.15],
-    [x2-0.15,y2-0.15]
+    [x1-0.0,y1-0.0],
+    [x2-0.0,y2-0.0]
   ]
 p.push(tr)
  let tr1 = [
-    [x1 + 0.15,y1+0.05],
-    [x2 + 0.15,y2+0.05]
-  ]
+   [x2 + 0.0,y2+0.0],
+    [x1 + 0.0,y1+0.0]
+    ]
   p.push(tr1)
 }
-
+rrect(4,1,13,6)
 trace(6.731,18.252,6.731,16.129)
 trace(6.731,16.129,8.731,14.129)
 trace(8.731,14.129,8.731,5.071)
@@ -102,3 +150,4 @@ expr(3.3,1.2,20.066,16.637,p)
 //const xyz = bt.merge(sc,p.lines())
 drawLines(t.lines());
 drawLines(p);
+drawLines(r.lines());
